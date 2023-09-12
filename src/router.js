093d287747df0ab5415953f229eccaaea83c58de
Rúben Freitas <joseruben98@hotmail.com>
2023-09-12 21:05:25 +0100

    new deploy test

--- src/router.js
+++ src/router.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Homepage from './components/Homepage';
 import Layout from "./components/Layout";
-import PageNotFound from './components/PageNotFound';
-import Wave from './components/Projects/Wave';
 
 function router() {
     return (
         <Layout>
             <Routes>
-                <Route exact path="/wavelabs" element={<Wave />} />
                 <Route exact path="/" element={<Homepage />} />
-                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </Layout>
     );
